6f72dde88bc034f0697e2139d295684a7e9e89f9
filipxd4 <filipwerochowski@icloud.com>
2019-10-01 23:14:59 +0200

    Dodaj pliki projektów.

---
+++ Risk_new/Controllers/HomeController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Risk_new.Models;
+
+namespace Risk_new.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Amount()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Amount(HomeViewModel model)
+        {
+            if (model.Azja == true)
+            {
+                model.Soliders = model.Soliders + 7;
+            }
+            //
+            if (model.Europa == true)
+            {
+                model.Soliders = model.Soliders + 5;
+            }
+            //
+            if (model.Afryka == true)
+            {
+                model.Soliders = model.Soliders + 3;
+            }
+            //
+            if (model.AmerykaPln == true)
+            {
+                model.Soliders = model.Soliders + 5;
+            }
+            //
+            if (model.AmerykaPld == true)
+            {
+                model.Soliders = model.Soliders + 2;
+            }
+            //
+            if (model.Australia == true)
+            {
+                model.Soliders = model.Soliders + 2;
+            }
+
+            model.Soliders = (model.Soliders + model.UnderHorse + model.Countries) / 3;
+
+            model.Score = Convert.ToInt32(model.Soliders);
+
+            ViewBag.Message = model.Score;
+
+            return View(model);
+        }
+    }
+}
---
+++ Risk_new/Models/HomeModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Risk_new.Models
+{
+    public class HomeViewModel
+    {
+        public float Soliders { get; set; }
+
+        public bool Azja { get; set; }
+
+        public bool Europa { get; set; }
+
+        public bool Australia { get; set; }
+
+        public bool AmerykaPld { get; set; }
+
+        public bool Afryka { get; set; }
+
+        public bool AmerykaPln { get; set; }
+
+        public float Countries { get; set; }
+
+        public float UnderHorse { get; set; }
+
+        public int Score { get; set; }
+    }
+}
